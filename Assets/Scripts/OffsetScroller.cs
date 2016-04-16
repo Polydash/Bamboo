47b030e0d729c7d42aa0881e45e4df92d02649cb
Arthur Décamp <arthur.decamp@free.fr>
2016-04-16 15:10:09 +0200

    Additional trucs and bidules about the scroller, you know ?

--- Assets/Scripts/OffsetScroller.cs
+++ Assets/Scripts/OffsetScroller.cs
@@ -3,20 +3,33 @@ using System.Collections;
 
 public class OffsetScroller : MonoBehaviour {
 
-	public float scrollSpeed;
-	private Vector2 savedOffset;
+	public float m_scrollSpeed;
+	public float m_xDir;
+	public float m_yDir;
+
+	private Vector2 m_savedOffset;
+
 
 	void Start () {
-		savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset ("_MainTex");
+		m_savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset ("_MainTex");
 	}
 
 	void Update () {
-		float x = Mathf.Repeat (Time.time * scrollSpeed, 1);
-		Vector2 offset = new Vector2 (x, savedOffset.y);
+		float x = m_savedOffset.x;
+		float y = m_savedOffset.y;
+		if (m_xDir == 1)
+		{
+			x = Mathf.Repeat (Time.time * m_scrollSpeed, 1);
+		}
+		if (m_yDir == 1)
+		{
+			y = Mathf.Repeat (Time.time * m_scrollSpeed, 1);
+		}
+		Vector2 offset = new Vector2 (x, y);
 		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", offset);
 	}
 
 	void OnDisable () {
-		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", savedOffset);
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", m_savedOffset);
 	}
 }
